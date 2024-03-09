cfddd82ddaac77e21f0f5e853444c80680873cb1
Justin Yee <justinyee1125@gmail.com>
2024-03-08 19:50:49 -0700

    Can now add pitcher stats to database based on team and season
    Can now add pitcher stats to database based on team and season. Still only gets pitcher stats. Can access the basketball reference website but can't scrape that website yet (headers for stats are hard-coded)

--- sqlQuerier.py
+++ sqlQuerier.py
@@ -3,12 +3,14 @@ import sqlite3
 
 import webScraper
 
+deleteAllRecordsPitcherTable = '''DELETE FROM pitchers
+                                '''
 
 createPitcherTable = '''CREATE TABLE pitchers (
                         pitcherId INTEGER NOT NULL PRIMARY KEY,
                         name TEXT,
                         age TEXT,
-                        games TEXT,
+                        games TEXT
                     )'''
                     
 createTeamsTable =  '''CREATE TABLE teams (
@@ -39,17 +41,21 @@ teams = [
 """
 
 with sqlite3.connect('baseballStats.db') as statsDb:
+    thisStatsScraper = webScraper.statsScraper()
+    baltimoreOriolesPitcherStats = thisStatsScraper.getBaseballTeamPitcherStats("Texas", "2023")
+    
     #statsDb.execute(createPitcherTable)
     #statsDb.execute(createTeamsTable)
-    
-    for (idx, pitcher) in enumerate(webScraper.pitchersStats):
+    statsDb.execute(deleteAllRecordsPitcherTable)
+    for (idx, pitcher) in enumerate(baltimoreOriolesPitcherStats):
         pitcherData = ((idx,) + pitcher)
-        print(pitcherData)
-        statsDb.execute(addPitcher,  pitcherData)
-        
+        #print(pitcherData)
+        statsDb.execute(addPitcher, pitcherData)
+""""    
     for team in teams:
         #print(team)
         statsDb.execute(addTeam, team)
+"""
  
         
 statsDb.commit()
--- webScraper.py
+++ webScraper.py
@@ -1,47 +1,96 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+import inspect
 
-url = "https://www.baseball-reference.com/teams/BAL/2023.shtml"
-r = requests.get(url)
-print(r)
 
-soup = BeautifulSoup(r.text, "html.parser")
-
-statsTable = soup.find("table", id="team_batting")
-
+"""
 statsTableHeaders = statsTable.findAll("th")
 headerNames = []
 for header in statsTableHeaders:
     headerNames.append(header.text)
 #print(headerNames)
-
-pitchersRows = []  
-statsTableRows = statsTable.findAll("tr")
-for row in statsTableRows[1:]:
-    try:
-        pos = row.find("td")
-        #print(pos.string)
-        if(pos.string == "P"):
-            pitchersRows.append(row)
-    except AttributeError:
+"""
+class statsScraper:
+    __URL_BASEBALL_STATS = "https://www.baseball-reference.com"
+    __URL_BASKETBALL_STATS = "https://www.basketball-reference.com"
+    __STR_BASEBALL = "Baseball"
+    __STR_BASKETBALL = "Basketball"
+    
+    def __init__(self) -> None:
         pass
-  
-pitchersStats = []
-  
-for pitcher in pitchersRows:
-    tempPitcher = ()
-    name = pitcher.find("td",{'data-stat':'player'})
-    tempPitcher = (*tempPitcher, name.text)
-    #print(pitcherName.text) 
-    age = pitcher.find("td",{'data-stat':'age'})
-    tempPitcher = (*tempPitcher, age.text)
-    games = pitcher.find("td",{'data-stat':'G'})
-    tempPitcher = (*tempPitcher, games.text)
-    pitchersStats.append(tempPitcher)
-"""
-for pitcher in pitchersStats:
-    print(pitcher)
-"""
+    
+                
+    def __getSoupTeamStats(self, sport, city, year):
+        ####todo scrape based on team param
+        url = ""
+        match sport:
+            case statsScraper.__STR_BASEBALL:
+                url = url + statsScraper.__URL_BASEBALL_STATS
+            case statsScraper.__STR_BASKETBALL:
+                url = url + statsScraper.__URL_BASKETBALL_STATS
+            case _:
+                nameOfEnclosingFunc = inspect.stack()[0][3]
+                statsScraper.__raiseErrorInvalidArg(nameOfEnclosingFunc)
+        
+        url = url + "/teams/"
+        
+        match city:
+            case "Baltimore":
+                url = url + "BAL"
+            case "Texas":
+                url = url + "TEX"
+            case _:
+                nameOfEnclosingFunc = inspect.stack()[0][3]
+                statsScraper.__raiseErrorInvalidArg(nameOfEnclosingFunc)
+                
+        url = url + f"/{year}"
+            
+        match sport:
+            case statsScraper.__STR_BASEBALL:
+                url = url + ".shtml"
+            case statsScraper.__STR_BASKETBALL:
+                url = url + ".html"
+            case _:
+                nameOfEnclosingFunc = inspect.stack()[0][3]
+                statsScraper.__raiseErrorInvalidArg(nameOfEnclosingFunc)    
+            
+        r = requests.get(url)
+        #print(r)
+        soup = BeautifulSoup(r.text, "html.parser")
+        return soup
+    
+    def getBaseballTeamPitcherStats(self, city, year):
+        soup = self.__getSoupTeamStats(statsScraper.__STR_BASEBALL, city, year)
+        statsTable = soup.find("table", id="team_batting")
+        pitchersRows = []  
+        statsTableRows = statsTable.findAll("tr")
+        for row in statsTableRows[1:]:
+            try:
+                pos = row.find("td")
+                #print(pos.string)
+                if(pos.string == "P"):
+                    pitchersRows.append(row)
+            except AttributeError:
+                pass
+        
+        pitchersStats = []
+        for pitcher in pitchersRows:
+            tempPitcher = ()
+            name = pitcher.find("td",{'data-stat':'player'})
+            tempPitcher = (*tempPitcher, name.text)
+            #print(pitcherName.text) 
+            age = pitcher.find("td",{'data-stat':'age'})
+            tempPitcher = (*tempPitcher, age.text)
+            games = pitcher.find("td",{'data-stat':'G'})
+            tempPitcher = (*tempPitcher, games.text)
+            pitchersStats.append(tempPitcher)
+        """
+        for pitcher in pitchersStats:
+            print(pitcher)
+        """
+        return pitchersStats
 
-
+    @classmethod
+    def __raiseErrorInvalidArg(nameOfEnclosingFunc):
+        raise ValueError(f'argument provided to function "{nameOfEnclosingFunc}" invalid')
