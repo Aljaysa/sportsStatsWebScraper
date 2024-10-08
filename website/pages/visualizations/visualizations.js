f2565f2b80c75fd50fa714de4af73e5cec22e995
Justin Yee <justinyee1125@gmail.com>
2024-10-08 08:04:43 -0600

    graph now shows up on website
    javascript file used to specify filepath of the graph

---
+++ website/pages/visualizations/visualizations.js
@@ -0,0 +1,8 @@
+function setStatsVisAttr(){
+    var statsVis = document.getElementById("stats-vis");
+    statsVis.src = "..\..\embeddedHTML\statsGraphs\WhiteSox2023AgeVsOPSPLUS.html";
+
+    statsVis.title = "Graph: White Sox 2023 Age Vs OPSPLUS" 
+}
+
+setStatsVisAttr();
