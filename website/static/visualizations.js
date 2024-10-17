05bfb8779f5cd092c7c5b69800558c0384e079fa
Justin Yee <justinyee1125@gmail.com>
2024-10-17 01:01:02 -0600

    flask web server is now used
    To run program:
    in Anaconda Prompt application navigate to file path and correct anaconda environment (sportsStatsWebScraper) and run:
    python webServer.py
    
    the full command line will look as follows:
    (sportsStatsWebScraper) C:\Users\justi\OneDrive\Desktop\CS\python\sportsStatsWebScraper\website>
    
    and then in web browser type into search bar:
    http://localhost:8000

--- website/static/visualizations.js
+++ website/static/visualizations.js
@@ -5,4 +5,12 @@ function setStatsVisAttr(){
     statsVis.title = "Graph: White Sox 2023 Age Vs OPSPLUS" 
 }
 
+fetch('/hello')
+    .then(function (response) {
+        return response.text();
+    }).then(function (text) {
+        console.log('GET response text:');
+        console.log(text); // Print the greeting as text
+    });
+
 setStatsVisAttr();
---
+++ website/webServer.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("visualizations/visualizations.html")
+
+@app.route("/visualizations")
+def visualizations():
+    return render_template("visualizations/visualizations.html")
+
+@app.route("/the-dev-journey")
+def theeDevJourney():
+    return render_template("the-dev-journey/the-dev-journey.html")
+
+
+@app.route('/visualizations', methods=['GET', 'POST'])
+def visualizationsMethods():
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'OK', 200
+
+# GET request
+    else:
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
