211ad3617a8b1bb888636a650ccf7c3c70f1a605
Justin Yee <justinyee1125@gmail.com>
2024-11-05 04:20:44 -0700

    url parameters generated
    url parameters generated to send to server for the graph to be displayed are derived from the user input (dropdown menus to select things like team, year, etc)

--- static/visualizations.js
+++ static/visualizations.js
@@ -1,18 +1,43 @@
 $( "document" ).ready( function setStatsVisAttr(){
     const statsVis = document.getElementById("stats-vis");
     
-    fetch('/visualizations/graph?team=blue_jays&year=2022&x_axis=age&y_axis=OPSPLUS&graph_type=scatterplot')
-    .then(function (response) {
-        return response.text();
-    }).then(function (text) {
-        console.log('GET response text:');
-        console.log(text); // Print the response text
-        statsVis.srcdoc = text; //static/embeddedHTML/statsGraphs/WhiteSox2023AgeVsOPSPLUS.html
-        statsVis.title = "Graph: White Sox 2023 Age Vs OPSPLUS";
-    });
-    
-    
 });
+
+function displayBaseballVisGraph(){
+    const userParams = new FormData(document.getElementById("stats-graph-user-parms"));
+    userParams.delete("position");
+    
+    
+    const urlOfRequest = "/visualizations/graph" + getGraphURLParamsToRequestFromServer(Object.fromEntries(userParams), "scatterplot"); //'/visualizations/graph?team=blue_jays&year=2022&x_axis=age&y_axis=OPSPLUS&graph_type=scatterplot'
+
+    getGraphFromServerAndDisplay("stats-vis", urlOfRequest);
+}
+
+function getGraphURLParamsToRequestFromServer(userParams, graphType){ 
+    let urlParamsToRequestFromServer = "?" + (new URLSearchParams(userParams)).toString();
+    // for(const pair of userParams.entries()){
+    //     urlParamsToRequestFromServer = urlParamsToRequestFromServer.concat(pair[0], "=", pair[1], "&");
+    // }
+    urlParamsToRequestFromServer = urlParamsToRequestFromServer.concat("&graph_type=", graphType);
+    return urlParamsToRequestFromServer;
+}
+
+
+function getGraphFromServerAndDisplay(graphId, urlOfRequest){
+    const statsVis = document.getElementById(graphId);
+    try {
+        fetch(urlOfRequest)
+        .then(function (response) {
+            return response.text();
+        }).then(function (text) {
+            console.log('GET response text:');
+            console.log(text); // Print the response text
+            statsVis.srcdoc = text; //static/embeddedHTML/statsGraphs/WhiteSox2023AgeVsOPSPLUS.html
+        }); 
+    } catch (e) {
+        console.error(e);
+    }
+}
 
 
 
